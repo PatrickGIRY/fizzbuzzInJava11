f89cc4c4540739c3b8a78bce69443d8690a6cacf
Patrick GIRY <patrickgiry@gmail.com>
2018-10-10 17:02:18 +0200

    Refactoring add rules of FizzBuzz.

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,14 +1,65 @@
 package fizzbuzz;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 class FizzBuzz {
+    private final Rule[] rules;
+
+    FizzBuzz() {
+        rules = new Rule[]{
+                Rule.rule().isMultipleOf(3).thenReturn("Fizz").build(),
+                Rule.rule().isMultipleOf(5).thenReturn("Buzz").build()
+        };
+    }
+
     String print(int n) {
-        String result = "";
-        if (n % 3 == 0) {
-            result += "Fizz";
-        }
-        if (n % 5 == 0) {
-            result += "Buzz";
-        }
+        String result = Stream.of(rules)
+                .filter(rule -> rule.isMultipleOf(n))
+                .map(Rule::thenReturn)
+                .collect(Collectors.joining());
+
         return result.isEmpty() ? String.valueOf(n) : result;
+    }
+
+    private static class Rule {
+        private int multipleOf;
+        private String thenReturn;
+
+        private Rule(int multipleOf, String thenReturn) {
+            this.multipleOf = multipleOf;
+            this.thenReturn = thenReturn;
+        }
+
+        static Builder rule() {
+            return new Builder();
+        }
+
+        boolean isMultipleOf(int n) {
+            return n % multipleOf == 0;
+        }
+
+        String thenReturn() {
+            return thenReturn;
+        }
+
+        private static class Builder {
+            private int multipleOf;
+            private String thenReturn;
+
+            Builder isMultipleOf(int multipleOf) {
+                this.multipleOf = multipleOf;
+                return this;
+            }
+
+            Builder thenReturn(String thenReturn) {
+                this.thenReturn = thenReturn;
+                return this;
+            }
+
+            Rule build() {
+                return new Rule(multipleOf, thenReturn);
+            }
+        }
     }
 }
